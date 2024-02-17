7f5c526f3e7da7015ee5a21e0ce9189f96d109e3
Your Name <your@email.com>
2024-02-18 00:21:50 +0600

    ffff

--- src/components/pages/Elias_try/Elias_try.jsx
+++ src/components/pages/Elias_try/Elias_try.jsx
@@ -28,6 +28,26 @@ const Elias_try = () => {
       </div>
     </div>
 
+    <br />
+      <div className="md:flex">
+        <div className="md:shrink-0">
+          <img
+            className="h-48 w-full object-cover md:h-full md:w-48"
+            // src="./src/assets/backcover.png"
+            // src="../../../assets/person2.png"
+
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT60FMndiOx7N7c5oAxKqCoSuyVN6ls75c7W3Q9QbCH5g&s"
+            alt="Modern building architecture"
+          />
+        </div>
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+          <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+          <p className="mt-2 text-gray-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+        </div>
+      </div>
+    </div>
+
 </div>
 
   );
