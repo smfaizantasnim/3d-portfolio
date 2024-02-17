ca33973dad7ed3e617cb231e63b3ab08c4fbadc3
Your Name <your@email.com>
2024-02-17 11:20:38 +0600

    thaks

--- src/components/MyBook.jsx
+++ src/components/MyBook.jsx
@@ -47,7 +47,7 @@ function MyBook(props) {
   return (
     <HTMLFlipBook width={600} height={700} showCover="true">
       <Page number={1}>
-        <Cover coverImg={coverImg} title="My Portfolio" />
+        <Cover coverImg={coverImg} title="Sm Elias Portfolio" />
       </Page>
       <Page number={2}>
         <FirstPage />
